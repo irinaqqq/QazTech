a3cbda0eb3d35d8fab33712194d3d5beb42ef7f9
irinaqqq <samataskarbaiev@gmail.com>
2024-06-03 14:25:52 +0500

    prod6

--- QT_website/wsgi.py
+++ QT_website/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for QT_website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
-import os
-
+import os,sys
+virtenv = os.path.expanduser('~') + '/venv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    if sys.version.split(' ')[0].split('.')[0] == '3':
+        exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+    else:
+        execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+sys.path.append(os.path.expanduser('~'))
+sys.path.append(os.path.expanduser('~') + '/ROOT/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'QT_website.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QT_website.settings')
-
 application = get_wsgi_application()
+
+
