f0082688155a6a2cc063d1327af15b86c27cb988
irinaqqq <madiyaryep@gmail.com>
2025-04-09 13:24:29 +0500

    fixes

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ FROM python:3.13-slim
 # Устанавливаем рабочую директорию  
 WORKDIR /app  
   
+# Устанавливаем системные зависимости  
+RUN apt-get update \  
+    && apt-get install -y build-essential libpq-dev gcc \  
+    && rm -rf /var/lib/apt/lists/*  
+  
 # Копируем файлы проекта  
 COPY . /app  
   
--- QT_website/settings.py
+++ QT_website/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = 'django-insecure-4@#8^&*g3!$@1v2j0z5q3x@6b1c7!9h3r4f3g5$%&*j1@#k1j1
 # DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['20.121.43.134', '127.0.0.1', 'node52038-env-2926384.jcloud.kz', 'mytechnology.kz']
-
+# ALLOWED_HOSTS = ['20.121.43.134', '127.0.0.1', 'node52038-env-2926384.jcloud.kz', 'mytechnology.kz']
+ALLOWED_HOSTS = ['*']  
 
 # Application definition
 
