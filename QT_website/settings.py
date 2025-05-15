4613c1d89f0097e43a8523ee3a945160ef0e34d1
irinaqqq <madiyaryep@gmail.com>
2025-05-15 13:57:15 +0500

    fix

--- QT_website/settings.py
+++ QT_website/settings.py
@@ -93,9 +93,6 @@ DATABASES = {
         'PASSWORD': open('/run/secrets/db_password').read().strip(),
         'HOST': open('/run/secrets/db_host').read().strip(),
         'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',  # 👈 обязательно для Azure
-        }
     }
 }
 
--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-import os
-import django
-from django.core.mail import send_mail
+import psycopg2
+from psycopg2 import OperationalError
 
-# Настройка Django окружения (замените 'your_project_name.settings' на реальное имя вашего проекта)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QT_website.settings')
-django.setup()
-
-def send_test_email():
-    try:
-        send_mail(
-            'Test Email',
-            'This is a test email sent from Django.',
-            'info@qt.com.kz',  # Отправитель
-            ['madiyaryep@gmail.com'],  # Замените на реальный email получателя
-            fail_silently=False,
-        )
-        print("Test email sent successfully!")
-    except Exception as e:
-        print(f"Error sending test email: {e}")
-
-if __name__ == '__main__':
-    send_test_email()
+try:
+    cnx = psycopg2.connect(
+        user="useradmin",  # Обрати внимание: в Azure обычно user с @hostname
+        password="foZtox-simmyf-ryxni9",
+        host="database-postgres-test.postgres.database.azure.com",
+        port=5432,
+        database="my_test_db",
+    )
+    print("Connection successful")
+    cnx.close()
+except OperationalError as e:
+    print("Error connecting to PostgreSQL:", e)
